324e3a19ad89fbd31d1bceac9bfab039a852802b
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-19 20:49:31 +0530

    Update dev.py

--- Flare_Robot/modules/dev.py
+++ Flare_Robot/modules/dev.py
@@ -5,7 +5,7 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import SaitamaRobot
+import Flare_Robot
 
 from Flare_Robot import dispatcher
 from Flare_Robot.modules.helper_funcs.chat_status import dev_plus
