21e604026763de923d51196962b907930191f425
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-19 21:42:49 +0530

    Update dbfunctions.py

--- Flare_Robot/utils/dbfunctions.py
+++ Flare_Robot/utils/dbfunctions.py
@@ -92,3 +92,26 @@ async def save_couple(chat_id: int, date: str, couple: dict):
     await coupledb.update_one(
         {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
     )
+
+# Couple Chooser
+async def _get_oyo_lovers(chat_id: int):
+    lovers = await coupledb.find_one({"chat_id": chat_id})
+    if not lovers:
+        return {}
+    return lovers["couple"]
+
+
+async def get_oyo_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_oyo_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
+    )
