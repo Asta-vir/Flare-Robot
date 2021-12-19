2629c055492f4fc15a57341a55d770a2429a5b89
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-19 20:24:44 +0530

    Update chat_status.py

--- Flare_Robot/modules/helper_funcs/chat_status.py
+++ Flare_Robot/modules/helper_funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SaitamaRobot.modules import connection
+from Flare_Robot.modules import connection
 
 connected = connection.connected
