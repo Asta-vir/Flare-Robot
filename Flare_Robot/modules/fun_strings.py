2057bac7106213f8f892efeb9bc7992e6d2f0b43
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-27 16:42:51 +0530

    Update fun_strings.py

--- Flare_Robot/modules/fun_strings.py
+++ Flare_Robot/modules/fun_strings.py
@@ -144,6 +144,37 @@ SLAP_TEMPLATES = (
     "Goblin slayer slays {user2}",  # Goblin Slayer
 )
 
+GBAM = "<b>Beginning Of Global Bam For {user2}</b>  \n \nChat Id : <code>{chatid}</code> \nReason : <i>{reason}</i> \nGBammed By {user1}"
+
+
+GBAM_REASON = (
+    "sasta noob",
+    "sasta waifu stealer",
+    "sasta white-het hekur",
+    "sasta white-het codur",
+    "sasta white-het vala chintu",
+    "sasta hexa hekur",
+    "sasta hexa playur",
+    "sasta tiktokur💃🏾",
+    "sasta membor of team 7",
+    "sasta selmon boi",
+    "sasta fri-fire player",
+    "sasta chhota bhim",
+    "sasta chhapri",
+    "sasta jony sin",
+    "sasta chhapri nibba",
+    "sasta nibba",
+    "sasti ria chokroborti",
+    "sasti nibbi",
+    "sasta camper",
+    "sasta pubji mobeil player",
+    "Aine me khudko dekh ke hilane wala",
+    "Bts ka choda",
+    "Kabhi Kabhi lagta he apun hi bhagwan he",
+    "Aise hi sexy lag raha tha",
+    "Eminem ka choda",
+)
+
 PAT_TEMPLATES = (
     "{user1} pats {user2} on the head.",
     "*gently rubs {user2}'s head*.",
