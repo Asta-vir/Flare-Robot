afb306106e781fc5bf0308a786679a3b28be6571
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-18 17:40:50 +0530

    Update __init__.py

--- Flare_Robot/__init__.py
+++ Flare_Robot/__init__.py
@@ -73,7 +73,7 @@ if ENV:
     CERT_PATH = os.environ.get("CERT_PATH")
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
-    DB_URI = os.environ.get("DATABASE_URI", "postgres://cossaojk:apEf76LRIlm0RL_SUngGreMghS7Bm5fb@castor.db.elephantsql.com/cossaojk")
+    DB_URI = os.environ.get("DATABASE_URI", "postgres://iriinioe:1QP9rKfvmgdRfEYArrHQcjvIZ-u_KBvR@fanny.db.elephantsql.com/iriinioe")
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
