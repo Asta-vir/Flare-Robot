a883d75e78b781b84e07f7ba87a9860f9aafa6a1
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-19 19:31:26 +0530

    Update reverse.py

--- Flare_Robot/modules/reverse.py
+++ Flare_Robot/modules/reverse.py
@@ -187,7 +187,7 @@ Do you know that you can search an image with a link too? /reverse picturelink <
 __mod_name__ = "Reverse"
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    ["reverse", "grs"], reverse, pass_args=True, admin_ok=True
+    ["reverse", "pp", "grs"], reverse, pass_args=True, admin_ok=True
 )
 
 dispatcher.add_handler(REVERSE_HANDLER)
