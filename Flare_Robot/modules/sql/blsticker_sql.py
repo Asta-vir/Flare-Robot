d84584c4a2dcaf019283a67f78a1315bfa17a17b
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-21 21:55:20 +0530

    Update blsticker_sql.py

--- Flare_Robot/modules/sql/blsticker_sql.py
+++ Flare_Robot/modules/sql/blsticker_sql.py
@@ -27,7 +27,7 @@ class StickersFilters(BASE):
 class StickerSettings(BASE):
     __tablename__ = "blsticker_settings"
     chat_id = Column(String(14), primary_key=True)
-    blacklist_type = Column(Integer, default=1)
+    blacklist_type = Column(BigInteger, default=1)
     value = Column(UnicodeText, default="0")
 
     def __init__(self, chat_id, blacklist_type=1, value="0"):
