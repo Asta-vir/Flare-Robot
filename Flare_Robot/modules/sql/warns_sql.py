256b9b0689058d4edd51834845321735fa0eb294
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-21 21:44:25 +0530

    Update warns_sql.py

--- Flare_Robot/modules/sql/warns_sql.py
+++ Flare_Robot/modules/sql/warns_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from Flare_Robot.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
+from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 from sqlalchemy.dialects import postgresql
 
 
