4ab62c71c59a057eb4d067f0b2ef7e61f7cbb3df
TR Crazy ✠ <91002607+TRDevil@users.noreply.github.com>
2022-01-23 19:10:16 +0530

    Update alive.py

--- Flare_Robot/modules/alive.py
+++ Flare_Robot/modules/alive.py
@@ -1,18 +1,18 @@
 from telethon import events, Button, custom
 import re, os
-from Flare-Robot.events import register
-from Flare-Robot import telethn as tbot
-from Flare-Robot import telethn as tgbot
+from Flare_Robot.events import register
+from Flare_Robot import telethn as tbot
+from Flare_Robot import telethn as tgbot
 PHOTO = "https://telegra.ph/file/1cd6d62ef6e8843e6b1cb.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
-  PIKACHU = event.sender.first_name
-  PIKACHU = "**♡ I,m ғʟᴀʀᴇ ʀᴏʙᴏᴛ 愛 ** \n\n"
-  PIKACHU += "**♡ I'm Working With sᴇxʏ Speed**\n\n"
-  PIKACHU += "**♡ ғʟᴀʀᴇ: LATEST Version**\n\n"
-  PIKACHU += "**♡ My Creator: [ᴀsᴛᴀ](http://t.me/Chifuyu_Matsuno_Kun)\n\n"
-  PIKACHU += "**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @Freia_Support **\n"
-  PIKACHU += "**♡ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💘💘💘**\n"
+  FLARE = event.sender.first_name
+  FLARE = "**♡ I,m ғʟᴀʀᴇ ʀᴏʙᴏᴛ 愛 ** \n\n"
+  FLARE += "**♡ I'm Working With sᴇxʏ Speed**\n\n"
+  FLARE += "**♡ ғʟᴀʀᴇ: LATEST Version**\n\n"
+  FLARE += "**♡ My Creator: [ᴀsᴛᴀ](http://t.me/Chifuyu_Matsuno_Kun)\n\n"
+  FLARE += "**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @Freia_Support **\n"
+  FLARE += "**♡ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💘💘💘**\n"
 
   BUTTON = [[Button.url("🚑 Support", "https://t.me/Freia_Support"), Button.url("📢 Updates", "https://t.me/Freia_Updates")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
