5ce2ee0c9fe361ea02c9662e66b99ee7bb077021
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-24 17:04:09 +0530

    Update disasters.py

--- Flare_Robot/modules/disasters.py
+++ Flare_Robot/modules/disasters.py
@@ -705,7 +705,7 @@ Group admins/group owners do not need these commands.
  ╠ `/gban <id> <reason>`*:* Gbans the user, works by reply too
  ╠ `/ungban`*:* Ungbans the user, same usage as gban
  ╚ `/gbanlist`*:* Outputs a list of gbanned users
-Visit @{SUPPORT_CHAT} for more information.
+Visit @Freia_Support for more information.
 """
 
 DEV_HANDLER = CommandHandler(("addpiro", "addheal"), addpiro)
