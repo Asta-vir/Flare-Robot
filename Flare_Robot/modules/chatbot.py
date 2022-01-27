1c4149e6c7c3750d4ed8cae7ce1116378fa5a5c8
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-01-27 21:27:51 +0530

    Update chatbot.py

--- Flare_Robot/modules/chatbot.py
+++ Flare_Robot/modules/chatbot.py
@@ -40,7 +40,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Claire Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
+                "Flare Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -66,7 +66,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Claire Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
+                "Flare Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
