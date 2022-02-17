de740f69e3bbc82eb2d11ac31db650a6d2eb1a06
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-17 17:46:25 +0530

    Update lewd.py

--- Flare_Robot/modules/lewd.py
+++ Flare_Robot/modules/lewd.py
@@ -638,6 +638,8 @@ def baka(update, context):
     msg.reply_video(nekos.img(target))
 
 
+PHOTO = "https://telegra.ph/file/a5c08a3d10e1f99559f6b.jpg"
+
 @register(pattern=("/flare"))
 async def awake(event):
     TEXT = "**♡ I,ᴍ ʏᴏᴜʀ ʟᴏᴠᴇ ʙᴀʙᴇ's 愛** \n\n"
